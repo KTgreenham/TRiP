6c898a7b1eba6d99101f2501b50e2879069087ba
KTgreenham <katie.greenham@gmail.com>
2015-01-22 17:23:17 -0500

    Remove all

--- code/errorFunc.m
+++
@@ -1,8 +0,0 @@
-% compute the RMS error between the current model and the data. This is
-% used by the non-linear optimization in modelFit.m
-%
-
-function[err] = errorFunc( model, dat )
-N   = length(dat);
-f   = evaluateModel( model, N );
-err = sum( (f - dat).^2 );
